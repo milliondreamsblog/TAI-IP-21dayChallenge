5083382ea9158945567155022d51b24213dcfa04
milliondreamsblog <114696765+milliondreamsblog@users.noreply.github.com>
2024-01-11 21:05:42 +0530

    Create DAY2.js
    DAY 2

---
+++ DAY2.js
@@ -0,0 +1,16 @@
+let intergerA = 10;
+let intergerB = 20;
+
+let StringA = "Namaste!";
+let StringB = "World!";
+
+let booleanvariable = true;
+
+let sumOfInt = intergerA+intergerB;
+let concatinationOfstring = StringA+StringB;
+booleanvariable =false;
+
+//Output 
+console.log("Sum of the two numbers-->",sumOfInt);
+console.log("Concatination of Strings--> ",concatinationOfstring);
+console.log(booleanvariable);
