baaa3892ceab3ce163b91856b9d12f9c8265b581
milliondreamsblog <114696765+milliondreamsblog@users.noreply.github.com>
2024-01-13 16:05:48 +0530

    Day4

---
+++ DAY4.js
@@ -0,0 +1,13 @@
+//for loop 
+for (let index = 0; index <= 10; index++) {
+    console.log("Printed from for loop"+index);
+    
+}
+
+//while loop 
+let index =0;
+while (index<=10) {
+    console.log("Printed form while loop"+index);
+    index = index +1;
+}
+
